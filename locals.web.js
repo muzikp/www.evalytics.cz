ae5e15dc5706b8da32bd9751862124098f11b2f7
muzikp <muzikp@gmail.com>
2024-03-15 18:32:02 +0100

    init

---
+++ locals.web.js
@@ -0,0 +1,18 @@
+/* this module is accessible from each pug file */
+
+module.exports = {
+    menu: [
+        {
+            id: "about",
+            label: "o nás",
+            href: "#o-nas"
+        },
+        {
+            id: "services",
+            label: "naše služby",
+            href: "#sluzby"
+        }
+    ],
+    message404: "To, co hledáte, tu není."
+
+}
