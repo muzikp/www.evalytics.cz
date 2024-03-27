22ae20b34ecd3eb179cd960f68684656dd88bc7d
muzikp <muzikp@gmail.com>
2024-03-27 20:09:27 +0100

    ...

--- locals.web.js
+++ locals.web.js
@@ -11,6 +11,107 @@ module.exports = {
             id: "services",
             label: "naše služby",
             href: "#sluzby"
+        },        
+        {
+            id: "people",
+            label: "lidé",
+            href: "#tym"
+        },
+        {
+            id: "reference",
+            label: "reference",
+            href: "#reference"
+        },
+        {
+            id: "contact",
+            label: "kontakt",
+            href: "#kontakt"
+        }        
+    ],
+    reference: [
+        {
+            id: "louda-auto",
+            alt: "Louda Auto a.s.",
+            img: "louda.svg",
+            url: "https://autolouda.cz/"
+        },
+        {
+            id: "car4way",
+            alt: "CAR4WAY a.s.",
+            img: "car4way.svg",
+            url: "https://www.car4way.cz/"
+        },
+        {
+            id: "bohemia-crystal",
+            alt: "CRYSTAL BOHEMIA a.s.",
+            img: "bohemia-crystal.webp",
+            url: "https://www.crystal-bohemia.com/"
+        },
+        {
+            id: "uk",
+            alt: "Univerzita Karlova",
+            img: "uk.webp",
+            url: "https://www.cuni.cz/"
+        },
+        {
+            id: "ministerstvo-zdravotnictvi",
+            alt: "Ministerstvo zdravotnictví České republiky",
+            img: "ministerstvo-zdravotnictvi.svg",
+            url: "https://www.mzcr.cz/"
+        },
+        {
+            id: "npi",
+            alt: "Národní pedagogický institut České republiky",
+            img: "npi.svg",
+            url: "https://www.npi.cz/"
+        },
+        {
+            id: "cvt",
+            alt: "Člověk v tísni",
+            img: "clovekvtisni.webp",
+            url: "https://www.clovekvtisni.cz/"
+        },
+        {
+            id: "testujto",
+            alt: "Testuj.to s.r.o.",
+            img: "testujto.svg",
+            url: "https://www.testuj.to/"
+        },
+        {
+            id: "zeny",
+            alt: "Ženy s.r.o.",
+            img: "zenysro.webp",
+            url: "https://www.zenysro.cz/"
+        },
+        {
+            id: "acomware",
+            alt: "ACOMWARE s.r.o.",
+            img: "acomware.svg",
+            url: "https://www.acomware.cz/"
+        },
+        {
+            id: "radenska",
+            alt: "Radenska d.o.o.",
+            img: "radenska.webp",
+            url: "https://www.radenska.si/"
+        },
+        {
+            id: "kofola",
+            alt: "Kofola ČeskoSlovensko a.s.",
+            img: "kofola.webp",
+            url: "https://www.kofola.cz/"
+        },
+        {
+            id: "kofola",
+            alt: "Kofola ČeskoSlovensko a.s.",
+            img: "kofola.webp",
+            url: "https://www.kofola.cz/"
+        },
+        {
+            id: "amber-plasma",
+            alt: "Amber Plasma a.s.",
+            img: "amber-plasma.svg",
+            url: "https://www.amberplasma.com/"
         }
     ],
     message404: "To, co hledáte, tu není."
